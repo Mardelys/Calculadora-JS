6f1c3fc172fcfc4ef760428961a61b33f69768ca
mardelys <mardelys1996@gmail.com>
2022-11-03 16:35:10 -0500

    Commit Avances

---
+++ Display.js
@@ -0,0 +1,8 @@
+class Display{
+    constructor(displayBValue, displayAValue){
+        this.displayAValue = displayAValue;
+        this.displayBValue = displayBValue;
+        this.calculadora = new Calculator();
+        this.valor
+    }
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+const displayBValue = document.getElementById('b-value');
+const displayAValue = document.getElementById('a-value');
+const buttonNumber = document.querySelectorAll ('.number');
+const buttonOpera = document.querySelectorAll('.opera');
+
+
+const Calculadora = new Calculadora();
+
+console.log(calculadora.sumar(2,3))
+console.log(calculadora.restar(2,3))
+console.log(calculadora.multiplicar(2,3))
+console.log(calculadora.dividir(2,3))
